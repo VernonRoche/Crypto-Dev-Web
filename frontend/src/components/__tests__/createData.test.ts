29d8040530a3e7f2f4f5f91662cc53af59765d12
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2022-12-04 16:35:50 +0100

    correction erreur lint

--- frontend/src/components/__tests__/createData.test.ts
+++ frontend/src/components/__tests__/createData.test.ts
@@ -1,29 +1,38 @@
-import { test, it, expect } from 'vitest'
+import { test } from "vitest";
 import { UserModel } from "@/models/user.model";
 import { connect, disconnect } from "@/database";
-test('create data for database test',async() => {
-    connect();
+test("create data for database test", async () => {
+  connect();
 
-    const users = [
-        {mail: "test@gmail.com",  firstname: "jack",  lastname: "lawson",  age: "20"},
-        {mail: "test22@gmail.com",  firstname: "elise",  lastname: "moss",  age: "80"},
-        {mail: "aaa@gmail.com",  firstname: "1",  lastname: "23",  age: "-99"},
-    ];
+  const users = [
+    {
+      mail: "test@gmail.com",
+      firstname: "jack",
+      lastname: "lawson",
+      age: "20",
+    },
+    {
+      mail: "test22@gmail.com",
+      firstname: "elise",
+      lastname: "moss",
+      age: "80",
+    },
+    { mail: "aaa@gmail.com", firstname: "1", lastname: "23", age: "-99" },
+  ];
 
-    try {
-        for (const user of users) {
-            await UserModel.create(user);
-            console.log(`Created user ${user.firstname} ${user.lastname}`);
-        }
-
-        for (const user of users) {
-            new Promise(resolve => UserModel.remove(user.firstname));
-            console.log(`Deleted user ${user.firstname} ${user.lastname}`);
-        }
-
-        disconnect();
-    } catch (error) {
-        console.error(error);
+  try {
+    for (const user of users) {
+      await UserModel.create(user);
+      console.log(`Created user ${user.firstname} ${user.lastname}`);
     }
 
-})
+    for (const user of users) {
+      new Promise(() => UserModel.remove(user.firstname));
+      console.log(`Deleted user ${user.firstname} ${user.lastname}`);
+    }
+
+    disconnect();
+  } catch (error) {
+    console.error(error);
+  }
+});
