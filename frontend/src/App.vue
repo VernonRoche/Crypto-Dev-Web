c7c17431995c5310724d7e012b895cb01d00cb39
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2022-12-14 18:52:14 +0100

    ajout fetch API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+<div>
 <link rel="stylesheet" href="@/assets/style.css">
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -23,4 +23,5 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
   </body>
   <RouterView />
+</div>
 </template>
