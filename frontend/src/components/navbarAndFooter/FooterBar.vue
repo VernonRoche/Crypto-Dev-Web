72f3896e9b90303ebabf44e88c20fc78eae12919
Alex Stergiou <44172223+VernonRoche@users.noreply.github.com>
2022-12-24 20:42:25 +0100

    Feature/8 topbottom bar (#18)
    * Added better header with logo, login/register buttons. Further styling to be done later on. Added Tailwind theme to tailwind.config.js
    
    * Added basic footer with "back to top" button. Better styling for header (applied theme).
    
    * Added page name to header.
    
    * Moved name left and changed logo to image instead of vector

---
+++ frontend/src/components/navbarAndFooter/FooterBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer aria-label="Site Footer" class="bg-gray-100 dark:bg-gray-900">
+    <div
+      class="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24"
+    >
+      <div
+        class="absolute top-4 right-4 sm:top-6 sm:right-6 lg:top-8 lg:right-8"
+      >
+        <a
+          class="inline-block rounded-full bg-teal-600 p-2 text-white shadow transition hover:bg-teal-500 dark:bg-gray-700 dark:text-teal-300 dark:hover:bg-gray-600 sm:p-3 lg:p-4"
+          href=""
+        >
+          <span class="sr-only">Back to top</span>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="#c084fc"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
+
+      <div class="lg:flex lg:items-end lg:justify-between">
+        <div>
+          <p
+            class="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 dark:text-gray-400 lg:text-left"
+          >
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
+            consequuntur amet culpa cum itaque neque.
+          </p>
+        </div>
+
+        <nav aria-label="Footer Nav" class="mt-12 lg:mt-0">
+          <ul
+            class="flex flex-wrap justify-center gap-6 md:gap-8 lg:justify-end lg:gap-12"
+          ></ul>
+        </nav>
+      </div>
+
+      <p
+        class="mt-12 text-center text-sm text-gray-500 dark:text-gray-400 lg:text-right"
+      >
+        Copyright &copy; 2022. All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterBar",
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/navbarAndFooter/NavbarHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <header aria-label="Site Header" class="bg-white dark:bg-gray-900">
+    <div
+      class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+    >
+      <a class="flex items-center text-purple-500" href="/">
+        <span class="sr-only">Home</span>
+        <img
+          class="h-8 w-auto"
+          src="https://media.discordapp.net/attachments/1042336221948551168/1054513351763439727/imageedit_8_5179903478.png"
+          alt="Logo"
+        />
+      </a>
+
+      <div
+        class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+      >
+        <h1 class="text-4xl font-extrabold text-primary content-center">
+          Cryptohub
+        </h1>
+      </div>
+
+      <div class="flex flex-1 items-center justify-end md:justify-between">
+        <nav aria-label="Site Nav" class="hidden md:block"></nav>
+
+        <div class="flex items-center gap-4">
+          <div class="sm:flex sm:gap-4">
+            <a class="btn btn-default" href="/"> Login </a>
+
+            <a class="btn btn-primary" href="/"> Register </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavbarHeader.vue",
+});
+</script>
+
+<style scoped></style>
