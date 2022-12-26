aa841daadb851281403fa266c0a63434e433eec4
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2022-12-26 18:17:45 +0100

    ajout favori

--- frontend/src/components/CryptoList.vue
+++ frontend/src/components/CryptoList.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="body">
       <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-        <table id="CryptoList" class="overflow-x-auto w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <table id="CryptoList" class="table-fixed overflow-x-auto text-sm text-left text-gray-500 dark:text-gray-400 whitespace-nowrap">
           <caption></caption>
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                   <th scope="col" class="py-3 px-6">
                     <div class="flex items-center">
                       Name
-                      <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
+                      <img src="@/assets/sort.png" alt="sort icon" class="object-scale-down h-4 w-4" />
                     </div>
                   </th>
                   <th scope="col" class="py-3 px-6">
                       <div class="flex items-center">
                           Price
-                          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
+                      <img src="@/assets/sort.png" alt="sort icon" class="object-scale-down h-4 w-4" />
                       </div>
                   </th>
                   <th scope="col" class="py-3 px-6">
                       <div class="flex items-center">
                           Variation
-                          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
+                      <img src="@/assets/sort.png" alt="sort icon" class="object-scale-down h-4 w-4" />
                       </div>
                   </th>
                   <th scope="col" class="py-3 px-6">
                       <div class="flex items-center">
                           Volume 24h
-                          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg></a>
+                      <img src="@/assets/sort.png" alt="sort icon" class="object-scale-down h-4 w-4" />
                       </div>
                   </th>
                   <th scope="col" class="py-3 px-6">
@@ -36,12 +36,10 @@
           </thead>
         </table>
       </div>
-      <CryptoDetails />
   </div>
 </template>
 <script lang="ts">
 import { CoinGeckoApi } from "../stores/CoinGeckoApi";
-import CryptoDetails from "../components/CryptoDetail.vue" 
 function createCryptoList(currency:string="usd"):HTMLTableSectionElement{
   let tbody:HTMLTableSectionElement = document.createElement("tbody");
   const data = CoinGeckoApi.getCryptoMarket();
@@ -83,26 +81,27 @@ function createCryptoList(currency:string="usd"):HTMLTableSectionElement{
       row.setAttribute("class","bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600");
 
       let nameDiv:HTMLTableCellElement = document.createElement("td");
-      nameDiv.setAttribute("class","py-4 px-6");
+      nameDiv.setAttribute("class","space-x-5 py-4 px-6");
       let img:HTMLImageElement = document.createElement("img");
       img.setAttribute("width","20");
       img.setAttribute("class","inline");
-      img.setAttribute("alt","image description");
+      img.setAttribute("alt","icon");
       img.src = element["image"];
       let name:HTMLParagraphElement = document.createElement("strong");
-      name.setAttribute("class","font-semibold text-gray-900 dark:text-white");
+      name.setAttribute("class","py-4 px-6");
       name.innerHTML=element["name"];
       let symbol:HTMLParagraphElement = document.createElement("p");
-      symbol.setAttribute("class","py-4 px-6");
-      symbol.innerHTML=element["symbol"]; 
+      symbol.setAttribute("class","inline font-semibold text-gray-900 dark:text-white");
+      symbol.innerHTML = element["symbol"]; 
       nameDiv.appendChild(img);
-      nameDiv.appendChild(name);
       nameDiv.appendChild(symbol);
+      nameDiv.appendChild(name);
+      
 
 
       let price:HTMLTableCellElement = document.createElement("td");
-      price.setAttribute("class","py-4 px-6");
-      price.innerHTML=element["current_price"] + " " +currency;
+      price.setAttribute("class","content-center py-4 px-6");
+      price.innerHTML=element["current_price"]+" "+ currency;
 
       let variation:HTMLTableCellElement = document.createElement("td");
       variation.setAttribute("class","py-4 px-6");
@@ -116,15 +115,34 @@ function createCryptoList(currency:string="usd"):HTMLTableSectionElement{
       detail.setAttribute("class","cursor-pointer font-medium text-blue-600 dark:text-blue-500 hover:underline");
       detail.innerHTML = "Details";
       detail.addEventListener("click", () => {
-        CryptoDetails.methods.ShowDetails();
+        //toDO
       });
+
+      let fav = document.createElement("td")
+      fav.setAttribute("class","py-4 px-6");
+      let favIcon:HTMLImageElement = document.createElement("img");
+      favIcon.setAttribute("alt","NotFav");
+      favIcon.src = "/src/assets/starEmpty.png";
+      fav.appendChild(favIcon);
+      fav.addEventListener('click', () => {        
+        if(favIcon.alt == "NotFav"){
+          //addToFavorite(); //TODO
+          favIcon.setAttribute("alt","Fav");
+          favIcon.src = "/src/assets/starFull.png";
+        }else{
+          //removeFromFavorite(); //TODO
+          favIcon.setAttribute("alt","NotFav");
+          favIcon.src = "/src/assets/starEmpty.png";
+        } 
+      });
+      
 
       row.appendChild(nameDiv);
       row.appendChild(price);
       row.appendChild(variation);
       row.appendChild(volume);
       row.appendChild(detail);
-
+      row.appendChild(fav);
       tbody.appendChild(row);
 
     });
@@ -133,6 +151,8 @@ function createCryptoList(currency:string="usd"):HTMLTableSectionElement{
 
   return tbody;
 }
+
+
 
 export default   
 {
@@ -145,7 +165,6 @@ export default
       const selectedCurrency:string= currencyDiv.options[currencyDiv.selectedIndex].text;
       document.getElementById("CryptoList")?.appendChild(createCryptoList(selectedCurrency));
     }
-
 }
 }
 
