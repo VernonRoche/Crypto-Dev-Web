accdf3bd0a79263ea2005e105ff7898e9f77a115
Alex Stergiou <44172223+VernonRoche@users.noreply.github.com>
2022-12-29 19:05:03 +0100

    Feature/22 mise en forme headerfooter trend et liste (#23)
    * Remade appearance of cryptolist. Removed details row, added fake column so that search bar is above crypto logo. Moved star icons into vue components.
    
    * Added trending crypto carousel. Added color change on hover, for list items.
    
    * Removed classes only chosen in darkmode. Now everything is darkmode permanently.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import NavbarHeader from "@/components/navbarAndFooter/NavbarHeader.vue";
 import FooterBar from "@/components/navbarAndFooter/FooterBar.vue";
-import CryptoTrend from "@/components/api/CryptoTrend.vue";
-import CryptoBar from "@/components/cryptoList/CryptoBar.vue";
+import CryptoList from "@/components/cryptoList/CryptoList.vue";
+import TrendCarousel from "@/components/TrendCarousel.vue";
 </script>
 <template>
   <link rel="stylesheet" href="@/assets/style.css" />
@@ -10,8 +10,8 @@ import CryptoBar from "@/components/cryptoList/CryptoBar.vue";
     <NavbarHeader />
   </header>
   <body>
-    <CryptoTrend />
-    <CryptoBar />
+    <TrendCarousel />
+    <CryptoList />
   </body>
   <footer>
     <FooterBar />
