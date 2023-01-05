1a0925026ad99491eec9a9c4757c27bb5b469282
Hichem Chouchane <hichem.chouchane@etu.u-bordeaux.fr>
2023-01-05 12:10:18 +0100

    graph v3

--- frontend/src/components/CryptoChart.vue
+++ frontend/src/components/CryptoChart.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-      <Line v-if="loaded" :data="chartData" :options="options" />
-      <div id="chartTimeButton" class="inline-flex">
+    <div id="chart-container" class="container h-50 w-50">
+      <Line id="CryptoChart" v-if="loaded" :data="chartData" :options="options" ref="line"/>
+      <div id="legendBox">
+      </div>
+    </div>
+      <div id="chartTimeUnit" class="inline-flex">
         <button v-on:click="changeTimeUnit('day')" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-l">
           Jours
         </button>
@@ -15,7 +19,8 @@
           Années
         </button>
       </div>
-  </div>
+
+      </div>
 </template>
 
 <script lang="ts">
@@ -23,138 +28,155 @@ import {createCryptoData}  from "@/stores/CryptoChartData";
 import { Line } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 import 'chart.js/auto';
+import {option} from "@/stores/chartConfig.ts";
+import zoomPlugin from 'chartjs-plugin-zoom';
+import AboutView from '../views/AboutView.vue';
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+ChartJS.register(zoomPlugin);
+/*
+1Jour : 
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setHours( dateTest.getHours() - 24);
+5 jour: 
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setDate( dateTest.getDate() - 5 );
+1 mois : 
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setMonth( dateTest.getMonth() - 1 );
+6 mois :
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setMonth( dateTest.getMonth() - 6 );
+1 an :
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setFullYear( dateTest.getFullYear() - 1 );
+5 ans : 
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setFullYear( dateTest.getFullYear() - 5 );
+ALL
+*/
+function changeTime(range:number=1){
+  let dateTest:Date = new Date();
+  let pastDate = new Date();
+  pastDate.setFullYear( dateTest.getFullYear() - 1 );
+  console.log(dateTest);
+  
+  console.log(pastDate);
+}
+
 function changeTimeUnit(unit:string='day'){
-  if(this.chartData){
-    this.options = {
-      plugins: {
-          title: {
-              text: "Bitcoin Chart",
-              display: true
-          }
-      },
-      scales: {
-          x: {
-              type: 'time',
-              time: {
-                  unit: unit
-              },
-              title: {
-                  display: true,
-                  text: 'Date'
-              }
-          },
-          y: {
-              title: {
-                  display: false,
-                  text: 'value'
-              }
-          }
-      },
-      responsive: true,
-      lineTension: 1,
-      maintainAspectRation: true,
-    };
+  if(this.chartData){    
+    for (const key in ChartJS.instances) {
+        if(ChartJS.instances[key].canvas.id == "CryptoChart"){
+          ChartJS.instances[key].options.scales["x"]["time"]["unit"] = unit;
+          ChartJS.instances[key].update();
+        }
+    }
   }
-
-
 }
 function randomColor(){
   return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6,'0').toUpperCase(); 
 }
+
 async function changeData(id:string=null,currency:string=null,from:number=null,to:number=null){
   if(id == null || currency == null){
     //initialisation de la page
     this.loaded = false;
     try {
-      await createCryptoData().then( (data) => {        
-        let datasetList:Array<Object> = []; 
+      await createCryptoData().then( (data) => {
+        let datasetList:Array<Object> = [];
         for (const key in data[0]) {
-          datasetList.push(          
-          {
-            label: key,
-            data: data[0][key],
-            backgroundColor: randomColor(),
-            borderColor: randomColor(),
-            color: randomColor(),
+          
+          let id:number = datasetList.length;
+          let backgroundColor = randomColor();
 
+            datasetList.push(          
+            {
+              label: key,
+              data: data[0][key],
+              backgroundColor: backgroundColor,
+              borderColor: randomColor(),
+              color: randomColor(),
+              lineTension: 0,
+
+            });
+          
+
+          let buttonLegend = document.createElement("button");
+          // id="CryptoChartPrice" v-on:click="toggleData(0)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full hidden"
+          buttonLegend.setAttribute("v-on:click","toggleData(0)");
+          buttonLegend.setAttribute("class"," hover:bgtext-white font-bold py-2 px-4 rounded-full");
+          buttonLegend.innerHTML = key;
+          buttonLegend.style.backgroundColor = backgroundColor;
+          buttonLegend.addEventListener("click",  () => {
+            for (const key in ChartJS.instances) {
+              if(ChartJS.instances[key].canvas.id == "CryptoChart"){
+                const  isVisible:boolean =  ChartJS.instances[key].isDatasetVisible(id);
+                if(isVisible){
+                  ChartJS.instances[key].hide(id);
+                }
+                if(!isVisible){
+                  ChartJS.instances[key].show(id);
+                } 
+              }
+            }
           });
+          document.getElementById("legendBox").appendChild(buttonLegend);
         }
         this.chartData = {
           labels: data[1]["prices"],
           datasets: datasetList,  
         }; 
-        this.loaded = true; 
+        this.loaded = true;
       });
     } catch (error) {
       console.error(error);
     }
-  }else{
-    //demande d'affichage d'une crypto
-    console.log(id);
-    this.options.plugins["title"]["text"] = id + " chart";
-    this.loaded = false;
-    try {
-      await createCryptoData(id,currency,from,to).then( (data) => {
-        let datasetList:Array<Object> = []; 
-        for (const key in data[0]) {
-          datasetList.push(          
-          {
-            label: key,
-            data: data[0][key],
-            backgroundColor: randomColor(),
-            borderColor: randomColor(),
-            color: randomColor(),
-          });
-        }        
-        this.chartData = {
-          labels: data[1]["prices"],
-          datasets: datasetList,  
-        }; 
-        this.loaded = true; 
-      });
-    } catch (error) {
-      console.error(error);
+  }else{    
+    //demande d'affichage d'une crypto 
+    for (const key in ChartJS.instances) {
+      if(ChartJS.instances[key].canvas.id == "CryptoChart"){        
+          ChartJS.instances[key].options.plugins["title"]["text"] = id + " chart";
+
+          try {
+            await createCryptoData(id,currency,from,to).then( (data) => {
+              let datasetList:Array<any> = []; 
+              for (const key in data[0]) {
+                datasetList.push(          
+                {
+                  label: key,
+                  data: data[0][key],
+                  backgroundColor: randomColor(),
+                  borderColor: randomColor(),
+                  color: randomColor(),
+                });
+              }
+              ChartJS.instances[key].data.labels =  data[1]["prices"];
+              ChartJS.instances[key].data.datasets = datasetList;
+            });
+          } catch (error) {
+            console.error(error);
+          }
+          ChartJS.instances[key].update();
+      }
     }
   }
 }
 export default{
+  //extends: Line,
   name: "CryptoChart",
   components: { Line },
-  //props: ['options'] ,
+  props: ['data'],
   data(){
     return {
       loaded: false,
       chartData: null,
-      options: {
-      plugins: {
-          title: {
-              text: "Bitcoin Chart",
-              display: true
-          }
-      },
-      scales: {
-          x: {
-              type: 'time',
-              time: {
-                  unit: 'day'
-              },
-              title: {
-                  display: true,
-                  text: 'Date'
-              }
-          },
-          y: {
-              title: {
-                  display: false,
-                  text: 'value'
-              }
-          }
-      },
-      responsive: true,
-      lineTension: 1,
-      maintainAspectRation: true,
-    },
+      options: option,
     }
   },
   /*
@@ -168,9 +190,16 @@ export default{
   methods: {
     changeTimeUnit: changeTimeUnit,
     changeData: changeData,
+    changeTime: changeTime,
   },
-  async mounted(){
+  async mounted(){    
     this.changeData();
-  }
+    for (const key in ChartJS.instances) {
+      if(ChartJS.instances[key].canvas.id == "CryptoChart"){ 
+    //CryptoChartMarketCap
+    //CryptoChartTotalVolume
+      }
+    }  
+  },
 }
 </script>
--- frontend/src/stores/CryptoChartData.ts
+++ frontend/src/stores/CryptoChartData.ts
@@ -1,10 +1,15 @@
 import { CoinGeckoApi }  from "../stores/CoinGeckoApi"
 import 'chartjs-adapter-luxon';
 
-export async function createCryptoData(id:string="bitcoin",currency:string="usd",from:number=1392577232/*new Date().setHours(0,0,0,0).valueOf()*/,to:number=Date.now()) {
+export async function createCryptoData(id:string="bitcoin",currency:string="usd",from:any=null/*new Date().setHours(0,0,0,0).valueOf()*/,to:any=null) {
     let datasets:Array<object>  = [];
     let labels:Array<string> = [];
-    const data  =  CoinGeckoApi.getCryptoMarketChartRange(id,currency,from,to);
+    let data = null;
+    if(from == null || to ==null ){
+        data  =  CoinGeckoApi.getCryptoMarketChartRange(id,currency,1392577232,Date.now());
+    }else{
+         data  =  CoinGeckoApi.getCryptoMarketChartRange(id,currency,from,to);
+    } 
     await data.then( (value:Array<string>) => {
         for(const key in value){
             let datas:Array<number> = [];
@@ -16,17 +21,21 @@ export async function createCryptoData(id:string="bitcoin",currency:string="usd"
                 market_caps
                 total_volumes
             */
+            
             value[key].forEach(element => {
                 datas.push(element[1]);
                 label.push(new Date(element[0]));
             });
             labels[key] = label; 
             datasets[key] = datas 
+         
         }
     });
+    /*
     labels.sort((a:Date,b:Date) =>{
         return a.getTime() - b.getTime();
     });
+    */
     return [datasets,labels];
 }
 
