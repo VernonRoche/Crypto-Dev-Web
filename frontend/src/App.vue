e786bc35e9fd7ffae98abb05a20f1172e0210a45
Alexandros Stergiou <jutaroroche@gmail.com>
2023-01-09 16:40:10 +0100

    Merged with dev branch, made CSS for value ranges.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,7 @@ import NavbarHeader from "@/components/navbarAndFooter/NavbarHeader.vue";
 import FooterBar from "@/components/navbarAndFooter/FooterBar.vue";
 import CryptoList from "@/components/cryptoList/CryptoList.vue";
 import TrendCarousel from "@/components/TrendCarousel.vue";
-import CryptoTrend from "@/components/api/CryptoTrend.vue";
-import SearchBox from "@/components/SearchBox.vue";
-import CryptoList from '@/components/CryptoList.vue';
-import Filter from '@/components/Filter.vue';
-import CryptoChart from "@/components/CryptoChart.vue";
-import BarChart from "@/components/Bar.vue";
+import CryptoChart from "@/components/chart/CryptoChart.vue";
 </script>
 <template>
   <link rel="stylesheet" href="@/assets/style.css" />
@@ -16,16 +11,9 @@ import BarChart from "@/components/Bar.vue";
     <NavbarHeader />
   </header>
   <body>
+    <CryptoChart />
     <TrendCarousel />
     <CryptoList />
-    <CryptoChart />
-    <div class="grid ">
-      <div class="content-center whitespace-nowrap space-x-10" >
-        <SearchBox />
-        <Filter />
-      </div>
-      <CryptoList />
-    </div>
   </body>
   <footer>
     <FooterBar />
