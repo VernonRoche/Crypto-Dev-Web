a3bd0f45d0ea939a4adf2f3a846eb93dca2ddfb5
vinsam33 <66140590+vinsam33@users.noreply.github.com>
2023-01-15 23:23:01 +0100

    tests

--- backend/test/1_api.test.ts
+++ backend/test/1_api.test.ts
@@ -16,14 +16,14 @@ describe('DBcommand', function() {
             const user = await DBcommand.getUser("0123456789",2);
             let result = JSON.parse(JSON.stringify(user));
 
-            // assert.notEqual(result.user_id, null);
-            // assert.notEqual(result.email, null);
-            // assert.notEqual(result.favorite, null);
-            // assert.notEqual(result.notification, null);
-            // assert.notEqual(result._id, undefined);
-            // assert.notEqual(result.email, undefined);
-            // assert.notEqual(result.favorite, undefined);
-            // assert.notEqual(result.notification, undefined);
+             assert.notEqual(result.user_id, null);
+             assert.notEqual(result.email, null);
+             assert.notEqual(result.favorite, null);
+             assert.notEqual(result.notification, null);
+             assert.notEqual(result._id, undefined);
+             assert.notEqual(result.email, undefined);
+             assert.notEqual(result.favorite, undefined);
+             assert.notEqual(result.notification, undefined);
 
             assert.equal(result.user_id, "0123456789");
             assert.equal(result.email, "test@crypto.fr");
@@ -32,11 +32,7 @@ describe('DBcommand', function() {
 
 
         });
-    });
 
-   
-    // test la fonction deleteUser
-    describe('deleteUser', function() {
         it('should delete a user', async function() {
             await DBcommand.deleteUser("0123456789",2) ;
             const user = await DBcommand.getUser("0123456789", 2);
@@ -46,11 +42,68 @@ describe('DBcommand', function() {
             // assert.deepEqual(result.favorite, null);
             // assert.deepEqual(result.notification, null);
         });
+
+        it('should add and get  favorite', async function() {
+            await DBcommand.insertUser("01234", "test2@crypto.fr",[""],[""],2);
+            await DBcommand.addFavorite("01234", "crypto",2);
+            const user = DBcommand.getFavorite("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.deepEqual(result.favorite, ["crypto"]);
+          });
+
+
+        it('should remove a favorite', async function() {
+            await DBcommand.removeFavorite("01234", "test",2);
+            const user = DBcommand.getFavorite("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.deepEqual(result.favorite, []);
+        });
+
+
+        it('change email', async function() {
+            await DBcommand.changeEmail("01234", "toto@test.com",2);
+            const user = DBcommand.getUser("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.notEqual(result.email, "test2@crypto.fr");
+            assert.equal(result.email, "toto@test.com");
+
+        });
+
+
+        it('add and get  notification', async function() {
+            await DBcommand.addNotification("01234", "test",200,2);
+            const user = DBcommand.getNotification("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.deepEqual(result.notification, ["200"]);
+
+        });
+
+
+        it('remove a notification', async function() {
+            await DBcommand.removeNotification("01234", "test",2);
+            const user = DBcommand.getNotification("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.deepEqual(result.notification, []);
+
+
+        });
+
+
+
+        it('should resetNotification a notification', async function() {
+            await DBcommand.resetNotification("01234",2);
+            const user = DBcommand.getNotification("01234",2);
+            let result = JSON.parse(JSON.stringify(user));
+            assert.deepEqual(result.notification, []);
+
+
+        });
+
+
+
     });
 
-    
-
-
+   
 });
 
 
