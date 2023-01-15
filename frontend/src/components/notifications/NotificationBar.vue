c79c764dde7b0063cb543b134ec092d86a9eab16
Alexandros Stergiou <jutaroroche@gmail.com>
2023-01-15 15:48:29 +0100

    Made inputs and buttons for possible notifications (without functions)

--- frontend/src/components/notifications/NotificationBar.vue
+++ frontend/src/components/notifications/NotificationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-2xl font-bold text-secondary overline content-center w-full h-full my-3 py-3"
+    class="text-3xl font-bold text-secondary underline underline-offset-8 content-center w-full h-full my-3 py-3"
     style="text-align: center"
   >
     Receive a price notification
@@ -10,19 +10,52 @@
     <div
       class="grid h-20 flex-auto w-1/3 card bg-base-300 rounded-box place-items-center"
     >
-      after x increase
+      After increase of
+      <div class="form-control">
+        <label class="input-group">
+          <button class="btn btn-accent btn-outline">GO</button>
+          <input
+            type="text"
+            placeholder="3"
+            class="input input-bordered input-accent"
+          />
+          <span class="badge-accent">%</span>
+        </label>
+      </div>
     </div>
     <div class="divider divider-horizontal">OR</div>
     <div
       class="grid h-20 flex-auto w-1/3 card bg-base-300 rounded-box place-items-center"
     >
-      after x decrease
+      After decrease of
+      <div class="form-control">
+        <label class="input-group">
+          <button class="btn btn-accent btn-outline">GO</button>
+          <input
+            type="text"
+            placeholder="3"
+            class="input input-bordered input-accent"
+          />
+          <span class="badge-accent">%</span>
+        </label>
+      </div>
     </div>
     <div class="divider divider-horizontal">OR</div>
     <div
       class="grid h-20 flex-auto w-1/3 card bg-base-300 rounded-box place-items-center"
     >
-      after reaching x value
+      After reaching
+      <div class="form-control">
+        <label class="input-group">
+          <button class="btn btn-accent btn-outline">GO</button>
+          <input
+            type="text"
+            placeholder="100"
+            class="input input-bordered input-accent"
+          />
+          <span class="badge-accent">USD</span>
+        </label>
+      </div>
     </div>
   </div>
 </template>
