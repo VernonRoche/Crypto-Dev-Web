77bc3fea5a9157eea6802e2ea400624c7b024c33
vsamson <vincent.samson@u-bordeaux.fr>
2023-01-16 07:08:58 +0100

    commentaire en+

--- frontend/src/stores/counter.ts
+++ frontend/src/stores/counter.ts
@@ -9,20 +9,36 @@ export function increment() {
   count.value++;
 }
 
+/**
+ * Count the number of Favorite for the current User and increment the counter with google
+ */
 export function incrementgoogle() {
   countsignGoogle.value++;
 }
 
+/**
+ * Count the number of Favorite for the current User and decrement the counter with google
+ */
 export function decrementGoogle() {
   countsignGoogle.value--;
 }
 
+/**
+ * Count the number of Favorite for the current User and decrement
+ */
 export function decrement() {
   count.value--;
 }
 
+/**
+ * Count the number of Favorite for the current User and increment the counter with google
+ */
 export const countsignGoogle = ref(0);
 countsignGoogle.value = 0;
+
+/**
+ * check if the user is connected with google
+ */
 export const isconnectedwithgoogle = ref(false);
 
 export default { count, increment, decrement };
